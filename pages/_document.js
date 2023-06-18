5c26242ed97f09af2c8820a08e2621497e057c53
Ahad69 <ahadxx99@gmail.com>
2023-06-18 16:52:33 +0600

    google analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,38 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 class WebDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
         <Head>
-        {/* <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-JNSZ5HRBP9"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+        
+           gtag('config', 'G-JNSZ5HRBP9');
+        `}
+          </Script>
+          {/* <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-5DTNFG5');`}}></Script> */}
-      
         </Head>
         <body>
-    
-
-            {/* <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DTNFG5"
+          {/* <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DTNFG5"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript> */}
-          <Main/>
+          <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
-export default WebDocument
+export default WebDocument;
