52b16cd10338979b3e1d693f6760ae2b7c0af66f
Ahad69 <ahadxx99@gmail.com>
2023-12-05 16:44:56 +0600

    add clarity

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,19 @@ class WebDocument extends Document {
            gtag('config', 'G-JNSZ5HRBP9');
         `}
           </Script>
+
+          <Script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "k1393958v9");
+                        `,
+            }}
+          />
           {/* <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
