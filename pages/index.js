4b6f4be39e996e7c396d7a34f4bfa48db48d8540
Ahad69 <ahadxx99@gmail.com>
2024-01-30 00:23:11 +0600

    fixed

--- pages/index.js
+++ pages/index.js
@@ -314,7 +314,7 @@ skip the games cape cod"
           type="application/javascript"
           src="https://bizhf.nxt-psh.com/ps/ps.js?id=49c_Gv6kp02qi7om3OJrlw"
         ></Script>
-        <Script type="text/javascript">
+        {/* <Script type="text/javascript">
           {`	atOptions = {
 		'key' : '76973f661d91255defc25e9ac01eed46',
 		'format' : 'iframe',
@@ -323,7 +323,7 @@ skip the games cape cod"
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/76973f661d91255defc25e9ac01eed46/invoke.js"></scr' + 'ipt>');`}
-        </Script>
+        </Script> */}
         <Footer />
       </main>
     </>
