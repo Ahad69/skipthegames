ed8bb59ecea96c858cc7bfcdae9460fdf688cbf0
Ahad69 <ahadxx99@gmail.com>
2024-02-19 00:42:04 +0600

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -18,16 +18,30 @@ export default function App({
         value={{ blogcurrent, setBlogCurrent, catKey, setCatKey }}
       >
         {/* amader ipp  */}
-        <Script
+        {/* <Script
           type="application/javascript"
           src="https://mzifx.ujscdn.com/ipp.js?id=3yFZb7MysUWZY7unZUeV2A&sub_id="
         ></Script>
+        <Script
+          type="application/javascript"
+          src="https://mzifx.nxt-psh.com/ps/ps.js?id=D-HxyA6RB0m9rBd4EGpOvw"
+        ></Script> */}
 
         {/* boss push ad  */}
         {/* <Script
           type="application/javascript"
           src="https://bizhf.nxt-psh.com/ps/ps.js?id=49c_Gv6kp02qi7om3OJrlw"
         ></Script> */}
+
+        {/* now  */}
+        <Script
+          type="application/javascript"
+          src="https://stumt.nxt-psh.com/ps/ps.js?id=ZlQAVEac70-Vs_K9IClrlg"
+        ></Script>
+        <Script
+          type="application/javascript"
+          src="https://stumt.ujscdn.com/ipp.js?id=5RiCTtRGgEi3dcYaD3gc-w&sub_id="
+        ></Script>
 
         <Component {...pageProps} />
       </MyContext.Provider>
