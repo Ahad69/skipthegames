263f73444e7a8c95b004fe8146706b34598e78c2
Ahad69 <ahadxx99@gmail.com>
2024-08-09 14:18:24 +0600

    updated

--- middleware.js
+++ middleware.js
@@ -2,9 +2,10 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    //"/dashboard/profile",
-    //"/user/post",
-    //"/user/local-ads",
-    //"/user/multiple-city-ads",
+    "/dashboard/profile",
+    "/dashboard/recharge",
+    "/user/post",
+    "/user/local-ads",
+    "/user/multiple-city-ads",
   ],
 };
